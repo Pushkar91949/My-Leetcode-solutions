9bcf5ee9eca125ba973782bc013b3973bc3e71f3
Pushkar91949 <117620508+Pushkar91949@users.noreply.github.com>
2024-02-23 23:25:00 +0530

    Create Happy number.py

---
+++ Happy number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next_number(num):
+            next_num = 0
+            while num > 0:
+                digit = num % 10
+                next_num += digit ** 2
+                num //= 10
+            return next_num
+
+        slow = n
+        fast = get_next_number(n)
+        while fast != 1 and slow != fast:
+            slow = get_next_number(slow)
+            fast = get_next_number(get_next_number(fast))
+
+        return fast == 1
