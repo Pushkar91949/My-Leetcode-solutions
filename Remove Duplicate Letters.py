b03b775fb4709d1ee6f6fe560517e6a9fbfabc43
Pushkar91949 <117620508+Pushkar91949@users.noreply.github.com>
2024-03-06 16:56:27 +0530

    Create Remove Duplicate Letters.py

---
+++ Remove Duplicate Letters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last_occurence = {char: index for index, char in enumerate(s)}
+        stack = []
+        seen = set()
+
+        for index, char in enumerate(s):
+            if char in seen:
+                continue
+            while stack and char < stack[-1] and index < last_occurence[stack[-1]]:
+                seen.remove(stack.pop())
+            stack.append(char)
+            seen.add(char)
+
+        return ''.join(stack)
