27a8ec50621f2b76dcf9224ea7b7fbe16346f966
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-03-10 11:24:04 +0530

    Update Intersection of Two arrays.py

--- Intersection of Two arrays.py
+++ Intersection of Two arrays.py
@@ -1,3 +1,4 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        return set(nums1) & set(nums2)
+        return list(set(nums1).intersection(nums2))
+# Question link: https://leetcode.com/problems/intersection-of-two-arrays/
