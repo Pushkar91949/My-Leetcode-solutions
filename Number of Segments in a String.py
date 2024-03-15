856bc00bf287c479c1bfe854df3e86f5b1810602
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-03-15 20:45:34 +0530

    Update Number of Segments in a String.py

--- Number of Segments in a String.py
+++ Number of Segments in a String.py
@@ -1,4 +1,19 @@
 class Solution:
     def countSegments(self, s: str) -> int:
-        x=s.split()
-        return len(x) #Question link: https://leetcode.com/problems/number-of-segments-in-a-string/
+        if not s:
+            return 0
+        
+        count = 0
+        in_segment = False
+        
+        for char in s:
+            if char != ' ':
+                if not in_segment:
+                    count += 1
+                    in_segment = True
+            else:
+                if in_segment:
+                    in_segment = False
+        
+        return count
+ #Question link: https://leetcode.com/problems/number-of-segments-in-a-string/
