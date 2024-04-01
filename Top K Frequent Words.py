8c28486641455b20f277db4fa4376934525a073e
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-04-01 14:28:44 +0530

    Update Top K Frequent Words.py

--- Top K Frequent Words.py
+++ Top K Frequent Words.py
@@ -1,5 +1,7 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         words_dict = Counter(words)
-        sorted_dict = [x[0] for x in sorted(words_dict.items(), key=lambda x:(-x[1], x[0]))]
-        return sorted_dict[0:k] # Question link: https://leetcode.com/problems/top-k-frequent-words/
+        heap = [(-freq, word) for word, freq in words_dict.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in range(k)]
+ # Question link: https://leetcode.com/problems/top-k-frequent-words/
