695346c58d1b09271608db612de568ffb708dc76
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-04-19 22:32:46 +0530

    Create Next Greater Element I.py

---
+++ Next Greater Element I.py
@@ -0,0 +1,23 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        if not nums2:
+            return None
+        
+        next_greater_map = {}
+        stack = []
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                top_element = stack.pop()
+                next_greater_map[top_element] = num
+            
+            stack.append(num)
+        
+        while stack:
+            top_element = stack.pop()
+            next_greater_map[top_element] = -1
+        
+        result = [next_greater_map[num] for num in nums1]
+        
+        return result
+ 
