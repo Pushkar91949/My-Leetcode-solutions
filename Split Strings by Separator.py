14c0474816dc97a3b5c17e82cdd83284d3639cf5
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-05-25 22:01:00 +0530

    Create Split Strings by Separator.py

---
+++ Split Strings by Separator.py
@@ -0,0 +1,16 @@
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        for i in range(len(words)):
+            words[i] = words[i].split(separator)
+        
+        ans = []
+        for word in words:
+            ans += word
+        
+        a = []
+        for word in ans:
+            if word != '':
+                a.append(word)
+
+        return a
+    # Question link: https://leetcode.com/problems/split-strings-by-separator/description/
