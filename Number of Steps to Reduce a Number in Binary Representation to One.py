a3e45493aa8d4b092fc61aabe0e870713c2a2c42
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-05-29 11:23:55 +0530

    Update Number of Steps to Reduce a Number in Binary Representation to One.py

--- Number of Steps to Reduce a Number in Binary Representation to One.py
+++ Number of Steps to Reduce a Number in Binary Representation to One.py
@@ -23,4 +23,27 @@ class Solution:
             steps += 1
         return steps
 ## NAIVE SOLUTION | BEATS 99%
+#############################################################################################
+# ANOTHER SOLUTION:
+class Solution:
+    def numSteps(self, s: str) -> int:
+        steps = 0
+        s = list(s)
+        
+        while len(s) > 1:
+            if s[-1] == '0':  
+                s.pop() 
+            else:  
+                i = len(s) - 1
+                while i >= 0 and s[i] == '1':
+                    s[i] = '0'
+                    i -= 1
+                if i >= 0:
+                    s[i] = '1'
+                else:
+                    s.insert(0, '1')
+            steps += 1
+        
+        return steps
+
 # Question link: https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/
