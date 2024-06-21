6a496ad1541e856ed9825a4971c371a8e13d654f
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-06-21 23:40:24 +0530

    Update 3Sum.py

--- 3Sum.py
+++ 3Sum.py
@@ -23,7 +23,6 @@ class Solution:
 # Time complexity = O(n^2)
 # O(nlogn) for sorting and O(n^2) when we run two loops, one inside another.) --> O(nlogn) + O(n^2) = O(n^2).
 
-
 # We take each element of nums and firs element of the triplet one by one. When the 1st element is set, we just need 2 more elements which we get by using 2 pointers, 
 # left and right (as we have already sorted the array). if sum of first element, nums[left] and nums[right] == 0: We add [a, nums[left], nums[right]] to our result array.
 # This process is done for each and every element when it is set as first element, it is not necessary that we find a solution for all the elements when they are set as first.
