fb942206f67cc392e75cc54dea0daed44e5d3c09
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-07-22 22:42:30 +0530

    Update Balanced Binary Tree.py

--- Balanced Binary Tree.py
+++ Balanced Binary Tree.py
@@ -7,4 +7,5 @@ class Solution:
             right = dfs(root.right)
             balanced = abs(left[1] - right[1]) <= 1 and left[0] and right[0]
             return [balanced, 1 + max(left[1],right[1])]
+            
         return dfs(root)[0]
