c50ad9e79c26b77516793d3c55b859a4a943ad5d
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-09-03 13:55:38 +0530

    Create Sum of Digits of String After Convert.py

---
+++ Sum of Digits of String After Convert.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getLucky(self, s: str, k: int) -> int:
+        letters = 'abcdefghijklmnopqrstuvwxyz'
+        mapp = {}
+        for i,char in enumerate(letters):
+            mapp[char] = str(i+1)
+        num = ''
+        for char in s:
+            num += mapp[char]
+        num = int(num)
+        def transform(num):
+            ans = 0
+            while num > 0:
+                ans += num % 10
+                num //= 10
+            return ans
+        for i in range(k):
+            num = transform(num)
+        return num
