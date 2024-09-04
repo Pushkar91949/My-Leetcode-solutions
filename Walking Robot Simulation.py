cc3c62d750042ae9cd87a31a831ab10dbb39d00b
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-09-04 19:05:16 +0530

    Create Walking Robot Simulation.py

---
+++ Walking Robot Simulation.py
@@ -0,0 +1,19 @@
+class Solution:
+    def robotSim(self, commands: List[int], obstacles: List[List[int]]) -> int:
+        x,y,d = 0,0,0
+        direct = [[0,1],[1,0],[0,-1],[-1,0]]
+        ans = 0
+        obstacles = {tuple(b) for b in obstacles}
+        for c in commands:
+            if c == -1:
+                d = (d + 1) % 4
+            elif c == -2:
+                d = (d - 1) % 4
+            else:
+                dx,dy = direct[d]
+                for _ in range(c):
+                    if (x+dx,y+dy) in obstacles:
+                        break
+                    x,y = x+dx, y+dy
+            ans = max(ans, x**2 + y**2)
+        return ans
