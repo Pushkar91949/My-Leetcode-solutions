0283cc4bd36d92025eacfc58cb6e682899ab2ea0
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2024-09-05 09:16:47 +0530

    Create Find Missing Observations.py

---
+++ Find Missing Observations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def missingRolls(self, rolls: List[int], mean: int, n: int) -> List[int]:
+        msum = sum(rolls)
+        m = len(rolls)
+        nsum = (mean * (n + m)) - msum
+        if nsum > n * 6 or nsum < n:
+            return []
+        nmean = nsum // n
+        nmod = nsum % n
+        ans = [nmean] * n
+        for i in range(nmod):
+            ans[i] += 1
+            
+        return ans
