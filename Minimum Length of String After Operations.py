432a049841e44f3067845c7a906fc1583ff3e9bf
Pushkar Joshi <117620508+Pushkar91949@users.noreply.github.com>
2025-01-13 22:14:00 +0530

    Create Minimum Length of String After Operations.py

---
+++ Minimum Length of String After Operations.py
@@ -0,0 +1,12 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        d = {}
+        for char in s:
+            d[char] = 1 + d.get(char, 0)
+        dele = 0
+        for char, count in d.items():
+            if count % 2 == 0:
+                dele += count - 2
+            else:
+                dele += count - 1
+        return len(s) - dele
